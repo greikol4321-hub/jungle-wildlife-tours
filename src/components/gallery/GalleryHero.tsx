884eb903c12119dc0b2b6d58bb82f6fd5226c25d
greikol4321-hub <greikol4321@gmail.com>
2026-07-19 23:40:12 -0600

    fix: separar texto del nav en tours galeria y contacto

--- src/components/gallery/GalleryHero.tsx
+++ src/components/gallery/GalleryHero.tsx
@@ -81,7 +81,7 @@ export function GalleryHero() {
       ))}
 
       <m.div
-        className="relative z-10 mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 pt-20 pb-16"
+        className="relative z-10 mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 pt-32 pb-16"
         style={{ opacity: contentOpacity, y: contentY }}
       >
         <div className="max-w-2xl">
